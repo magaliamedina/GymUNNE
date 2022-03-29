c181c7d38cc9984c4c7cb4fd7c885932e2c75810
Magali Medina <magalimedina96@gmail.com>
2022-03-29 20:50:06 -0300

    Se agregó filtro por fechas a reporte alumnos por grupo.

--- app/src/main/java/com/example/gimnasio_unne/view/fragments/FragmentReporteAlumnosPorGrupo.java
+++ app/src/main/java/com/example/gimnasio_unne/view/fragments/FragmentReporteAlumnosPorGrupo.java
@@ -2,6 +2,8 @@ package com.example.gimnasio_unne.view.fragments;
 
 import android.Manifest;
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
@@ -20,6 +22,7 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.Spinner;
@@ -57,8 +60,12 @@ import org.json.JSONArray;
 import org.json.JSONException;
 
 import java.lang.reflect.Array;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -66,12 +73,14 @@ import java.util.Map;
 //reporte en base a alumnos por grupo de todos los años
 public class FragmentReporteAlumnosPorGrupo extends Fragment {
 
-    private BarChart barChart, barChartMeses;
+    private BarChart barChart;
     private String URL="https://medinamagali.com.ar/gimnasio_unne/consulta_alumnos_por_grupo.php";
     private String total_reservas;
     private String[] grupos;
     private Button btnGenerar;
     private ProgressBar progressBar;
+    Button btnDesde, btnHasta, btnFiltrar;
+    DatePickerDialog datePickerDialog, datePickerDialog2;
     //creamos la lista con los valores de entrada
     List<BarEntry> entradas = new ArrayList<>();
 
@@ -88,6 +97,9 @@ public class FragmentReporteAlumnosPorGrupo extends Fragment {
         View view= inflater.inflate(R.layout.fragment_reporte_alumnos_por_grupo, container, false);
         barChart=view.findViewById(R.id.barChartAlumnosPorGrupo);
         btnGenerar=view.findViewById(R.id.btnGenerarPDF);
+        btnDesde=view.findViewById(R.id.btnDsdGrupos);
+        btnHasta=view.findViewById(R.id.btnHstGrupos);
+        btnFiltrar=view.findViewById(R.id.btnFiltrarGrupos);
         progressBar=view.findViewById(R.id.progressBarReportePorGrupo);
 
         //Permisos
@@ -98,10 +110,54 @@ public class FragmentReporteAlumnosPorGrupo extends Fragment {
             ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,},1000);
         }
 
+        try {
+            initDatePickerDesde();
+            initDatePickerHasta();
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        btnDesde.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                datePickerDialog.show();
+            }
+        });
+        btnHasta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                datePickerDialog2.show();
+            }
+        });
+
         //Genera el documento
         GenerarPDF generarPDF = new GenerarPDF();
         if (tieneConexionInternet()) {
-            mostrarDatos();
+            mostrarDatos("2021-11-2", Utiles.obtenerFechaActual("GMT -3"));
+            btnFiltrar.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    String desde = btnDesde.getText().toString();
+                    String hasta= btnHasta.getText().toString();
+                    if(desde.equals("Desde") || hasta.equals("Hasta"))
+                        Toast.makeText(getActivity(), "Debe ingresar fecha desde y hasta", Toast.LENGTH_SHORT).show();
+                    else {
+                        SimpleDateFormat dateFormat = new SimpleDateFormat ("yyyy-MM-dd");
+                        try {
+                            Date date1 = dateFormat.parse(desde);
+                            Date date2 = dateFormat.parse(hasta);
+                            if(date1.before(date2)) {
+                                mostrarDatos(desde, hasta);
+                                limpiaGraficoBarra();
+                            }
+                            else
+                                Toast.makeText(getActivity(), "Fecha hasta debe ser mayor a fecha desde", Toast.LENGTH_SHORT).show();
+                        } catch (ParseException e) {
+                            e.printStackTrace();
+                        }
+                    } // fin else
+                }
+            }); //FIL BOTON FILTRAR
+
             btnGenerar.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
@@ -111,6 +167,7 @@ public class FragmentReporteAlumnosPorGrupo extends Fragment {
                     Toast.makeText(getActivity().getApplicationContext(), "Se generó el PDF en la carpeta de Descargas", Toast.LENGTH_SHORT).show();
                 }
             });
+            //FIN SI TIENE CONEXION
         } else {
             progressBar.setVisibility(View.GONE);
             Toast.makeText(getActivity().getApplicationContext(), "No se pudo conectar, revise el " +
@@ -119,7 +176,7 @@ public class FragmentReporteAlumnosPorGrupo extends Fragment {
         return view;
     }
 
-    public void mostrarDatos() {
+    public void mostrarDatos(String desde, String hasta) {
         StringRequest request = new StringRequest(Request.Method.POST, URL, new Response.Listener<String>() {
             @Override
             public void onResponse(String response) {
@@ -143,7 +200,15 @@ public class FragmentReporteAlumnosPorGrupo extends Fragment {
             public void onErrorResponse(VolleyError error) {
                 Toast.makeText(getActivity().getApplicationContext(), error.getMessage(), Toast.LENGTH_SHORT).show();
             }
-        });
+        }){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> parametros = new HashMap<String, String>();
+                parametros.put("fecha_desde", desde);
+                parametros.put("fecha_hasta", hasta);
+                return parametros;
+            }
+        };
         RequestQueue requestQueue = Volley.newRequestQueue(getActivity().getApplicationContext());
         requestQueue.add(request);
     }
@@ -176,6 +241,20 @@ public class FragmentReporteAlumnosPorGrupo extends Fragment {
         //LAS 2 siguientes lineas son para que cargue los datos sin hacer clic en el grafico
         barChart.notifyDataSetChanged();
         barChart.invalidate();
+    }
+
+    private void limpiaGraficoBarra() {
+        //Elimina datos
+        entradas.clear();
+        //mandamos los datos para crear la gráfica
+
+        BarDataSet datos = new BarDataSet(entradas ,"");
+        BarData data = new BarData(datos);
+        barChart.setData(data);
+
+        barChart.invalidate();
+        barChart.clear();
+
     }
 
     private void legend(BarChart barChart) {
@@ -211,5 +290,61 @@ public class FragmentReporteAlumnosPorGrupo extends Fragment {
         return false;
     }
 
+    private void initDatePickerDesde() throws ParseException {
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month= month+1;
+                String date= year+"-"+month+"-"+day;
+                btnDesde.setText(date);
+            }
+        };
+        Calendar calendar = Calendar.getInstance();
+        int style= AlertDialog.THEME_HOLO_LIGHT;
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        datePickerDialog = new DatePickerDialog(getActivity(), style,dateSetListener,year,month,day);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        long dateMillis;
+        Date dateHoy;
+        dateHoy= sdf.parse(Utiles.obtenerFechaActual("GMT -3"));
+        dateMillis=dateHoy.getTime();
+        datePickerDialog.getDatePicker().setMaxDate(dateMillis);
 
+        datePickerDialog.getDatePicker().init(2022, 00, 01, new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker datePicker, int year, int month, int day) {
+            }
+        });
+    }
+
+    private void initDatePickerHasta() throws ParseException {
+        DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                month= month+1;
+                String date= year+"-"+month+"-"+day;
+                btnHasta.setText(date);
+            }
+        };
+        Calendar calendar = Calendar.getInstance();
+        int style= AlertDialog.THEME_HOLO_LIGHT;
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        datePickerDialog2 = new DatePickerDialog(getActivity(), style,dateSetListener,year,month,day);
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        long dateMillis;
+        Date dateHoy;
+        dateHoy= sdf.parse(Utiles.obtenerFechaActual("GMT -3"));
+        dateMillis=dateHoy.getTime();
+        datePickerDialog2.getDatePicker().setMaxDate(dateMillis);
+
+        datePickerDialog2.getDatePicker().init(year, month, day, new DatePicker.OnDateChangedListener() {
+            @Override
+            public void onDateChanged(DatePicker datePicker, int year, int month, int day) {
+            }
+        });
+    }
 }
